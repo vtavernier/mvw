c7605d4b991d113ab9731c45060c3341c14577fe
Vincent Tavernier <vincent.tavernier@inria.fr>
2019-01-30 15:40:13 +0100

    Fix grid cell center computation

--- src/glsl/gabor/grid.glsl
+++ src/glsl/gabor/grid.glsl
@@ -1,6 +1,3 @@
-#define off3(v) vec3(v.x<0?-.5:v.x==0?0:.5,v.y<0?-.5:v.y==0?0:.5,v.z<0?-.5:v.z==0?0:.5)
-#define off2(v) vec2(v.x<0?-.5:v.x==0?0:.5,v.y<0?-.5:v.y==0?0:.5)
-
 m4_define(GRID3D_DEFINE,`void $1(inout vec4 O, in vec3 P, in int max_disp, in vec3 tile_size) {
     ivec3 ccell = ivec3(P / tile_size + .5 * sign(P));
     ivec3 disp;
@@ -12,7 +9,7 @@ m4_define(GRID3D_DEFINE,`void $1(inout vec4 O, in vec3 P, in int max_disp, in ve
             // Current cell coordinates
             ivec3 cell = ccell + disp;
             // Cell center (pixel coordinates)
-            vec3 center = tile_size * (vec3(cell) + off3(cell));
+            vec3 center = tile_size * vec3(cell);
 
             $2(O, P, ccell, cell, center);
         }
@@ -28,7 +25,7 @@ m4_define(GRID2D_DEFINE,`void $1(inout vec4 O, in vec2 P, in int max_disp, in ve
         // Current cell coordinates
         ivec2 cell = ccell + disp;
         // Cell center (pixel coordinates)
-        vec2 center = tile_size * (vec2(cell) + off2(cell));
+        vec2 center = tile_size * vec2(cell);
 
         $2(O, P, ccell, cell, center);
     }
