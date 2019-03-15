7e53015a2b33a61f75df9f400819ca6d98f0722a
Vincent Tavernier <vincent.tavernier@inria.fr>
2019-03-15 16:36:02 +0100

    Implement noise prefiltering

--- src/glsl/util/gabor_main.glsl
+++ src/glsl/util/gabor_main.glsl
@@ -3,6 +3,8 @@
 //! vec2 gW0 min=0 max=360 fmt="%2.2f" cat="Gabor noise" unm="W0" ang
 //! bool dGrid def=false cat="Debug" unm="Show grid"
 
+//! float aSigma min=0.01 max=2.0 fmt="%2.3f" cat="Prefiltering" unm="Sigma" def=1.0 pow=2.0
+
 layout(location = 1) out vec4 fragLighting;
 
 void mainImage(out vec4 O, in vec2 U)
@@ -12,7 +14,15 @@ void mainImage(out vec4 O, in vec2 U)
 
     //debugRotGrid(O, vPosition, 0, _TILE_SIZE);
 
+    // Compute prefiltering at current pixel
+    vec3 n = normalize(vNormal);
     vec3 w0 = W0VEC(gW0);
+    vec3 w0p0 = w0 - dot(w0, n) * n;
+    float w0p0n = length(w0p0);
+
+    vec2 wP = 2. * M_PI * gF0 * vec2(length(dFdx(w0p0)), length(dFdy(w0p0)));
+    float f = exp(-pow(w0p0n * length(wP) / aSigma, 2.) / 2.);
+
     // Grid-evaluate Gabor noise
     gaborGrid(O, bboxMin + vPosition, 1, _TILE_SIZE, w0);
 
@@ -22,7 +32,11 @@ void mainImage(out vec4 O, in vec2 U)
     // Compute variance in O.b for renorm, using mipmap
     O.b = O.r * O.r;
 
+    // Apply prefiltering to the noise value
+    O.r *= f;
 
+    // Return filter strength in green channel
+    O.g = f;
 
     // [0, 1] range for the noise value
     O.r = .5 * O.r + .5;
