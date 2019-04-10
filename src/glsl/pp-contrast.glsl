98c2912db2b60d777c2376f93b12b27df5e2fa43
Vincent Tavernier <vincent.tavernier@inria.fr>
2019-04-10 16:08:43 +0200

    Adaptative contrast window first implementation

--- src/glsl/pp-contrast.glsl
+++ src/glsl/pp-contrast.glsl
@@ -4,10 +4,13 @@ m4_include(pp/lighting.glsl)
 //! float cFilterLod min=1.0 max=8.0 fmt="%2.2f" cat="Contrast correction" unm="C. LOD" def=2.0
 uniform float cFilterLod;
 
+//! bool cAdaptiveWindow cat="Contrast correction" unm="Adaptive window" def=1
+uniform bool cAdaptiveWindow;
+
 void mainImage(out vec4 O, in vec2 U)
 {
     vec4 c = texture(iChannel0, U / iResolution.xy);
-    vec4 fc = textureLod(iChannel0, U / iResolution.xy, cFilterLod);
+    vec4 fc = textureLod(iChannel0, U / iResolution.xy, (cAdaptiveWindow ? pow(c.g, 2.0) : 1.0) * cFilterLod);
 
     if (dGrid) {
         O = c;
