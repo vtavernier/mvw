20739a3b6c73ca6298514e821bdf456ee7f8b3e6
Vincent Tavernier <vincent.tavernier@inria.fr>
2019-05-14 16:55:07 +0200

    Remove unused GRID2D_DEFINE

--- src/glsl/gabor/grid.glsl
+++ src/glsl/gabor/grid.glsl
@@ -17,20 +17,3 @@ m4_define(GRID3D_DEFINE,`void $1(inout vec4 O, in vec3 P, in int max_disp, in ve
             $2(O, P, ccell, cell, center, w0);
         }
 }')
-
-m4_define(GRID2D_DEFINE,`void $1(inout vec4 O, in vec2 P, in int max_disp, in vec2 tile_size) {
-    ivec3 ccell = ivec3(P / tile_size + .5 * sign(P));
-    ivec2 disp;
-
-    for (disp.x = -max_disp; disp.x <= max_disp; ++disp.x)
-        for (disp.y = -max_disp; disp.y <= max_disp; ++disp.y)
-    {
-        // Current cell coordinates
-        ivec2 cell = ccell + disp;
-        // Cell center (pixel coordinates)
-        vec2 center = tile_size * vec2(cell);
-
-        $2(O, P, ccell, cell, center);
-    }
-}')
-
