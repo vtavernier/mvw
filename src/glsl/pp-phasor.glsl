08294209ebab3bf2d840dcd00b3ba582286a0fc1
Vincent Tavernier <vincent.tavernier@inria.fr>
2019-06-11 18:19:41 +0200

    Better phasor field + noise vis

--- src/glsl/pp-phasor.glsl
+++ src/glsl/pp-phasor.glsl
@@ -11,17 +11,21 @@ uniform bool dPhase;
 
 void mainImage(out vec4 O, in vec2 U)
 {
-    O = texture(colorOutput, U / iResolution.xy);
+    float a = .2;
+    vec4 C = O = texture(colorOutput, U / iResolution.xy);
     vec4 P = texture(phasorField, U / iResolution.xy); // phasor field
 
     O.rg = 2. * O.rg - 1.;
 
+    // Noise value
+    C.rgb = .5 + .5 * vec3(O.g / length(O.rg));
+
     if (dPhase) {
         float phase = mod(atan(P.g, P.r), 2. * M_PI) / (2. * M_PI);
-        O.rgb = length(P.rg) * cm_matlab_hsv(phase).rgb;
+        O.rgb = C.rgb * a + (1. - a) * (.45 * length(P.rg) + .5) * cm_matlab_hsv(phase).rgb;
     } else {
         // Show only noise value
-        O.rgb = .5 + .5 * vec3(O.g / length(O.rg));
+        O.rgb = C.rgb;
     }
 
     O.rgb = lighting(U, O.rgb);
